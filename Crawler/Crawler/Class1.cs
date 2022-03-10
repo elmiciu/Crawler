3c58e8ded709c063e940373621ef1ef586e23ac7
mpazio <mpazio@pjwstk.edu.pl>
2022-03-10 11:36:01 +0100

    Zadanie3

---
+++ Crawler/Crawler/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class Class1
+    {
+        private int _myProperty;
+        public int MyProperty { 
+            get { return _myProperty * 15; } 
+            set { _myProperty = value; } 
+        }
+        public int MyProperty2 { get; set; }
+    }
+}
